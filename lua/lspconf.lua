a10abb235dee9d82ae82ff954deaea69b6d5ad73
Matheus Henrique Passos de Oliveira <matheus.passos@ifgoiano.edu.br>
2022-11-23 11:09:07 -0300

    feat: add html support

--- lua/lspconf.lua
+++ lua/lspconf.lua
@@ -31,6 +31,13 @@ end),
 capabilities=require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 }
 
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+lspconfig.html.setup{
+    capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+}
+
 
 
 
