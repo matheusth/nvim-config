147a447fc9e621e63b07e1729c2f0938b800f96e
Matheus Henrique Passos de Oliveira <matheuspassos.ti@outlook.com>
2023-08-05 15:44:09 -0300

    clear all files

--- ftplugin/cmake.lua
+++
@@ -1,9 +0,0 @@
-local lspconfig = require('lspconfig')
-
-lspconfig.cmake.setup{
-    on_attach=(function(client, bufnr) 
-        vim.keymap.set("n", "<leader>ft", vim.lsp.buf.formatting, {buffer = bufnr})
-        vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {buffer = bufnr})
-    end),
-    capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-}
