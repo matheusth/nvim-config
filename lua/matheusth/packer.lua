91e6f1849021bd9e3fb40338ccea53c2d6547e47
Matheus Henrique Passos de Oliveira <matheuspassos.ti@outlook.com>
2023-08-06 18:35:53 -0300

    add vim-tmux-navigator and lualine

--- lua/matheusth/packer.lua
+++ lua/matheusth/packer.lua
@@ -33,4 +33,9 @@ return require('packer').startup(function(use)
 		  {'L3MON4D3/LuaSnip'},     -- Required
 	  }
   }
+ use 'christoomey/vim-tmux-navigator'
+ use {
+     'nvim-lualine/lualine.nvim',
+     requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+ }
 end)
