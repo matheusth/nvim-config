e8121c436d06a705230edb8f35594acb20b3ce3b
Matheus Henrique Passos de Oliveira <matheuspassos.ti@outlook.com>
2023-08-06 11:58:00 -0300

    removed tmux sessionizer

--- lua/matheusth/remap.lua
+++ lua/matheusth/remap.lua
@@ -1,14 +1,11 @@
 vim.g.mapleader = " ";
 
 
-vim.keymap.set("n","<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
 
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
