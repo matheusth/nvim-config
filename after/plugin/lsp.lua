c2c5dd7ae67c7ffe0fc636ae76aedc0620f51325
Matheus Henrique Passos de Oliveira <matheushtip@gmail.com>
2023-12-15 11:47:17 -0300

    feat: Add description to lsp keymaps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,14 +3,14 @@
 -- Set up lspconfig.
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local on_attach = (function()
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = 0 })
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = 0 })
-    vim.keymap.set('n', 'gT', vim.lsp.buf.type_definition, { buffer = 0 })
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { buffer = 0 })
-    vim.keymap.set('n', '<leader>df', vim.diagnostic.goto_next, { buffer = 0 })
-    vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, { buffer = 0 })
-    vim.keymap.set('n', '<leader>ft', vim.lsp.buf.format, { buffer = 0 })
-    vim.keymap.set('n', '<leader>vca', vim.lsp.buf.code_action, { buffer = 0 })
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = 0, desc="LSP hover." })
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = 0, desc="LSP go to definition." })
+    vim.keymap.set('n', 'gT', vim.lsp.buf.type_definition, { buffer = 0, desc="LSP type definition." })
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { buffer = 0, desc="LSP go to implementation." })
+    vim.keymap.set('n', '<leader>df', vim.diagnostic.goto_next, { buffer = 0, desc="Diagnostic go to next." })
+    vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, { buffer = 0, desc="LSP rename" })
+    vim.keymap.set('n', '<leader>ft', vim.lsp.buf.format, { buffer = 0, desc="LSP format" })
+    vim.keymap.set('n', '<leader>vca', vim.lsp.buf.code_action, { buffer = 0, desc="LSP code action" })
 end)
 
 -- (Optional) Configure lua language server for neovim
