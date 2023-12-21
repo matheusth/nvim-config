8fcfd553bfd7a3be17bd4dd97079cc62fa7f9a98
Matheus Henrique Passos de Oliveira <matheushtip@gmail.com>
2023-12-21 13:47:49 -0300

    dia 21

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,6 +3,7 @@ return {
 	config = function()
 		local lspconfig = require('lspconfig');
 		lspconfig.tsserver.setup {}
+    lspconfig.gopls.setup {}
 		lspconfig.lua_ls.setup {
 			on_init = function(client)
 				local path = client.workspace_folders[1].name
