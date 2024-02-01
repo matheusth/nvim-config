53516dc9b8a60d5ffcb82f4e24b61a299f1817fe
Matheus Henrique Passos de Oliveira <matheuspassos.ti@outlook.com>
2024-02-01 18:56:10 -0300

    added snippets for java files

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,19 +1,24 @@
 return {
-	"L3MON4D3/LuaSnip",
-	-- follow latest release.
-	version = "v2.1", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-	-- install jsregexp (optional!).
-	build = "make install_jsregexp",
-	config = function()
-		local ls =  require("luasnip")
-		local s = ls.snippet
-		local i = ls.insert_node
-		local fmt = require"luasnip.extras.fmt".fmt
-		vim.keymap.set({"i"}, "<c-k>", function() ls.expand() end, {silent = true})
-		vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump(1) end, {silent = true})
-		vim.keymap.set({"i", "s"}, "<C-J>", function () ls.jump(-1) end, {silent = true})
-		ls.add_snippets("all", {
-			s("lorem", fmt("lorem ipsum dolor sit ammet {}.",{i(0)}))
-		})
-	end
+    "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    version = "v2.1", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp",
+    config = function()
+        local ls = require("luasnip")
+        local s = ls.snippet
+        local i = ls.insert_node
+        local fmt = require "luasnip.extras.fmt".fmt
+        vim.keymap.set({ "i" }, "<c-k>", function() ls.expand() end, { silent = true })
+        vim.keymap.set({ "i", "s" }, "<C-L>", function() ls.jump(1) end, { silent = true })
+        vim.keymap.set({ "i", "s" }, "<C-J>", function() ls.jump(-1) end, { silent = true })
+        ls.add_snippets("all", {
+            s("lorem", fmt("lorem ipsum dolor sit ammet {}.", { i(0) }))
+        })
+        ls.add_snippets("java", {
+            s("pbc", fmt("public class {} {{\n{}\n}}", { vim.fn.expand("%:r"), i(1)})),
+            s("psvm", fmt("public static void main(String[] args){{\n{}\n}}", {i(0)})),
+            s("sysout", fmt("System.out.println(\"{}\");", {i(0)})),
+        })
+    end
 }
