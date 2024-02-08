13a34e26496c427266baa4d64903e56d82e6fcea
Matheus Henrique Passos de Oliveira <matheushtip@gmail.com>
2024-02-08 11:09:37 -0300

    feat: added nonels

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,12 @@
 return {
-	"nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
-	config=function()
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
 		local configs = require("nvim-treesitter.configs")
 		configs.setup({
-      auto_install = {enable = true} ,
-			highlight = {enable = true},
-			indent = {enable = true},
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
 		})
-	end
+	end,
 }
