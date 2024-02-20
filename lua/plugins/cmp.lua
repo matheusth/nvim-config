6581c30aeb6882858a01ef1fdb5d083acf23689e
Matheus Henrique Passos de Oliveira <matheushtip@gmail.com>
2024-02-20 11:19:42 -0300

    removed bordered window

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -23,7 +23,6 @@ return {
                 end,
             },
             window = {
-                completion = cmp.config.window.bordered(),
                 documentation = cmp.config.window.bordered(),
             },
             mapping = cmp.mapping.preset.insert({
