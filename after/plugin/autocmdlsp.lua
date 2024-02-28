59d7fa5b7f41da138fbb83b660941c19f0b35750
Matheus Henrique Passos de Oliveira <matheuspassos.ti@outlook.com>
2024-02-28 09:35:47 -0300

    feat: move all jdtls config into lua/plugins/jdtls.lua

--- after/plugin/autocmdlsp.lua
+++ after/plugin/autocmdlsp.lua
@@ -2,7 +2,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
     callback = function(ev)
         vim.keymap.set("n", "<leader>ft", vim.lsp.buf.format, { buffer = ev.buffer })
         vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer = ev.buffer})
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {buffer = ev.buffer})
         vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {})
         vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
     end,
