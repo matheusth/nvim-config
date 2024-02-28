9a26525df6c7b8e9cfc95b5cb5067ce556d31fbc
Matheus Henrique Passos de Oliveira <matheuspassos.ti@outlook.com>
2024-02-27 21:41:51 -0300

    feat: setup harpoon

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -2,7 +2,5 @@ return {
     'mbbill/undotree',
     config = function ()
         vim.keymap.set('n', '<leader><F2>', vim.cmd.UndotreeToggle, {})
-        
-        print("config undotree");
     end
 }
