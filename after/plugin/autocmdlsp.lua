ddda5c1f1a29ed8c97b01280241c3d2f176d4d9b
Matheus Henrique Passos de Oliveira <matheuspassos.ti@outlook.com>
2024-03-11 08:39:46 -0300

    fix: move jdtls back to ftplugin and add code action keybind

--- after/plugin/autocmdlsp.lua
+++ after/plugin/autocmdlsp.lua
@@ -4,5 +4,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer = ev.buffer})
         vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {})
         vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
     end,
 })
