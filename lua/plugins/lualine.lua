90c8ac4900ed3737a6c7b953f85acd6ec628c66a
Matheus Henrique Passos de Oliveira <matheuspassos.ti@outlook.com>
2026-04-09 07:58:03 -0300

    fix lualine theme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,7 @@ return {
 	config = function()
 		require'lualine'.setup({
 			options = {
-				theme = 'catppuccin',
+				theme = 'catppuccin-nvim',
 				section_separators = {left = '', right = ''},
 				component_separators = {left = '', right = ''},
 			},
